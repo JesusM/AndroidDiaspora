edb7ab9c2b594a7248d84c3be560a9dc11fcf478
jesus <jesus@jesus-Aspire-5742G>
2011-09-18 16:22:48 +0200

    Fixed message inbox bug (desactivated it) and other translations errors in UI.

--- src/org/jesus/Diaspora/VistaRecuperarContrasena.java
+++ src/org/jesus/Diaspora/VistaRecuperarContrasena.java
@@ -8,6 +8,6 @@ import android.os.Bundle;
 public class VistaRecuperarContrasena extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.vista_recuperacion_contrasena);
+		setContentView(R.layout.view_forgot_password);
 	}
 }
